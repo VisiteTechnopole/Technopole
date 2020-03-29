95da5e749f58523728dfcc5b5fde5d87c14fd35a
turkimajdi <56868685+turkimajdi@users.noreply.github.com>
2020-03-29 16:03:10 +0100

    ff

--- Visite/Assets/Users.cs
+++ Visite/Assets/Users.cs
@@ -1,12 +1,52 @@
-﻿using System.Collections;
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Users : MonoBehaviour
-{ 
+{
+    DatabaseReference reference;
+    public InputField inputField;
    
-    public void Onsubmit()
+
+  
+
+     void Start ()
     {
-        
+        Debug.Log("bb");
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://visitetechno.firebaseio.com/");
+
+        // Get the root reference location of the database.
+         reference = FirebaseDatabase.DefaultInstance.RootReference;
+        Debug.Log(reference);
     }
+
+
+    public void SaveData()
+    {
+
+        if (!inputField.text.Equals(""))
+        {
+            Debug.Log(inputField.text.ToString());
+            reference.Child("users1").Child("username").Child("Email").SetValueAsync(inputField.text.ToString());
+        }     
+    }
+    /*public void LoadData()
+    {
+        FirebaseDatabase.DefaultInstance.GetReference("users1").ValueChanged += FirebaseSaveLoadValue;
+    }
+    private void FirebaseSaveLoadValue(object sender ,ValueChangedEventArgs e)
+    {
+        if (e.Snapshot.Child("username").Exists)
+        {
+            text.text = e.Snapshot.Child("username").Child("Email").GetValue(true).ToString();
+        }
+    }*/
+
+
+
 }
