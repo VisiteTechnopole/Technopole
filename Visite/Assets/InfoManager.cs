9accb825be995629dcfc0bac09a84654e783aab4
turkimajdi <56868685+turkimajdi@users.noreply.github.com>
2020-04-05 23:00:45 +0100

    virtuel camera

--- Visite/Assets/InfoManager.cs
+++ Visite/Assets/InfoManager.cs
@@ -47,7 +47,7 @@ public class InfoManager : MonoBehaviour
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 1000))
+        if (Physics.Raycast(ray, out hit, 700))
         {
             Debug.Log("You selected the " + hit.transform.name);
             int index = infos.FindIndex(d => d.tag == hit.transform.tag);
