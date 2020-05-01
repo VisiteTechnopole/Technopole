2d64e2627a43b8b7d1701f3e66276942a4b31ac4
turkimajdi <56868685+turkimajdi@users.noreply.github.com>
2020-05-01 23:36:21 +0100

    add technopole1

--- Visite/Assets/InfoManager.cs
+++ Visite/Assets/InfoManager.cs
@@ -81,4 +81,5 @@ public class InfoManager : MonoBehaviour
     {
         _object.SetActive(false);
     }
+    
 }
---
+++ Visite/Assets/open.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void openPanel(GameObject _object)
+    {
+        _object.SetActive(true);
+    }
+    public void closePanel(GameObject _object)
+    {
+        _object.SetActive(false);
+    }
+}
